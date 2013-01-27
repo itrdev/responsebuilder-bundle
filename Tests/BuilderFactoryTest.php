910b74de40be522c3904eab90ed696e1f45903a2
Dmitry Nehaychik <4dmitr@gmail.com>
2013-01-27 23:03:41 +0300

    test json builder

--- Tests/BuilderFactoryTest.php
+++ Tests/BuilderFactoryTest.php
@@ -3,6 +3,7 @@
 namespace Itr\ResponseBuilderBundle\Tests;
 
 use Itr\ResponseBuilderBundle\ResponseBuilder\ResponseBuilderFactory;
+use Itr\ResponseBuilderBundle\ResponseBuilder\ParameterBag;
 
 class BuilderFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -26,5 +27,22 @@ class BuilderFactoryTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('Itr\ResponseBuilderBundle\Exception\InvalidBuilderFormatException', 'Invalid builder format: xml');
         $responseBuilder = new ResponseBuilderFactory('json');
         $responseBuilder->getBuilderForFormat('xml');
+    }
+
+    public function testJsonBuilder()
+    {
+        $responseBuilder = new ResponseBuilderFactory('json');
+        $jsonBuilder = $responseBuilder->getDefault();
+
+        $pb = new ParameterBag();
+        $value = 'some value';
+        $pb->set('first.second', $value);
+        /** @var \Symfony\Component\HttpFoundation\Response $response  */
+        $response = $jsonBuilder->build(200, $pb);
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertJsonStringEqualsJsonString($response->getContent(), json_encode(array('first' => array('second' => $value))));
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
     }
 }
